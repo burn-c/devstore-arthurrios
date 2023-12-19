e6954d7b91f02ce3e17e80fda87388bb57ea1f68
arthur <arthur.rios007@gmail.com>
2023-12-19 18:19:58 -0300

    add cypress add to cart test

---
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -0,0 +1,36 @@
+describe('add-product-to-cart', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('should be able to navigate to the product page and add it to the cart', () => {
+    cy.get('a[href^="/product"]').first().click()
+
+    cy.location('pathname').should('include', '/product')
+    cy.contains('Add to cart').click()
+
+    cy.contains('Cart (1)').should('exist')
+  })
+
+  it('should not count duplicated products in cart', () => {
+    cy.get('a[href^="/product"]').first().click()
+
+    cy.location('pathname').should('include', '/product')
+    cy.contains('Add to cart').click()
+    cy.contains('Add to cart').click()
+
+    cy.contains('Cart (1)').should('exist')
+  })
+
+  it('should be ablet o search for a product and add it to the cart', () => {
+    cy.get('input[name=q]').type('sweatshirt').parent('form').submit()
+
+    cy.get('a[href^="/product"]').first().click()
+
+    cy.location('pathname').should('include', '/product')
+
+    cy.contains('Add to cart').click()
+
+    cy.contains('Cart (1)').should('exist')
+  })
+})
